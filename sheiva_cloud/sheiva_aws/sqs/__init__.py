c066136efe0a079d88860009019920ee7778ea99
Dan <daniel.lenehan.work@gmail.com>
2023-09-03 11:48:40 +0100

    Cleanup

--- sheiva_cloud/sheiva_aws/sqs/__init__.py
+++ sheiva_cloud/sheiva_aws/sqs/__init__.py
@@ -1,5 +1,7 @@
-
 WORKOUTLINK_QUEUE = (
     "https://sqs.eu-west-1.amazonaws.com/381528172721/WorkoutLinkQueue.fifo"
 )
-WORKOUTLINK_DEADLETTER_QUEUE = "https://sqs.eu-west-1.amazonaws.com/381528172721/WorkoutLinkDeadLetterQueue.fifo"
+WORKOUTLINK_DEADLETTER_QUEUE = (
+    "https://sqs.eu-west-1.amazonaws.com/381528172721/"
+    "WorkoutLinkDeadLetterQueue.fifo"
+)
--- sheiva_cloud/sheiva_aws/sqs/sqs.py
+++ sheiva_cloud/sheiva_aws/sqs/sqs.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, Optional
 
 import boto3
 
@@ -13,9 +13,9 @@ class SQS:
 
     def __init__(
         self,
-        queue_url: str = None,
-        message_group_id: str = None,
-        boto3_session: boto3.Session = None,
+        queue_url: str,
+        message_group_id: Optional[str] = None,
+        boto3_session: Optional[boto3.Session] = None,
     ):
         self.sqs_session = boto3_session or boto3.Session().client("sqs")
         self.queue_url = queue_url
@@ -23,9 +23,9 @@ class SQS:
 
     def send_message(
         self,
-        message_body: str = None,
-        message_attributes: dict = None,
-        message_group_id: str = None,
+        message_body: str,
+        message_attributes: Optional[dict] = None,
+        message_group_id: Optional[str] = None,
     ) -> Dict:
         """
         Send a message to the queue.
@@ -45,13 +45,18 @@ class SQS:
         )
         return response
 
-    def receive_message(self, max_number_of_messages: int = 1) -> Dict:
+    def receive_message(
+        self, max_number_of_messages: Optional[int] = 1
+    ) -> Dict:
         """
         Receive messages from the queue.
         Args:
-            max_number_of_messages (int): The maximum number of messages to return.
-            We should be careful with requesting more than one message because deleting
-            a message from the queue is not atomic. If we request more than one message.
+            max_number_of_messages (int): The maximum number
+                of messages to return.
+            We should be careful with requesting more than
+                one message because deleting
+            a message from the queue is not atomic. If we
+                request more than one message.
         Returns:
             dict: The response from the SQS receive_message method.
         """
@@ -66,9 +71,11 @@ class SQS:
         """
         Delete a message from the queue.
         Args:
-            receipt_handle (str): The receipt handle of the message to delete.
-            This is different to the message id. The receipt handle represents
-            the specific instance of receiving the message versus a single message.
+            receipt_handle (str): The receipt handle of
+                the message to delete. This is different to
+                the message id. The receipt handle represents
+                the specific instance of receiving the
+                message versus a single message.
         Returns:
             dict: The response from the SQS delete_message method.
         """

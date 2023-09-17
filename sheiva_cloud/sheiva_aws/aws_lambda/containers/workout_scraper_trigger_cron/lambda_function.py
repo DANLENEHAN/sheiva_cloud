16c96e39059ea7620f367f32c5341c456c44dc46
Dan <daniel.lenehan.work@gmail.com>
2023-09-17 20:34:33 +0100

    WorkoutScraperTriggerCron lambda

---
+++ sheiva_cloud/sheiva_aws/aws_lambda/containers/workout_scraper_trigger_cron/lambda_function.py
@@ -0,0 +1,44 @@
+"""
+Lambda function for putting messages on the Workout Scraper Trigger SQS queue.
+Requires the following environment variables:
+    - NUMBER_WORKOUT_LINKS_PER_MESSAGE: number of workout links to put in each message
+"""
+
+import os
+import boto3
+
+from sheiva_cloud.sheiva_aws.sqs.standard_sqs import StandardSQS
+from sheiva_cloud.sheiva_aws.sqs import WORKOUT_SCRAPE_TRIGGER_QUEUE_URL
+
+NUMBER_WORKOUT_LINKS_PER_MESSAGE = os.getenv("NUMBER_WORKOUT_LINKS_PER_MESSAGE", None)
+
+# pylint: disable=unused-argument
+def handler(event, context):
+    """
+    Lambda handler for putting messages on the
+    workout scraper trigger SQS queue.
+    Args:
+        event (Dict): event object
+        context (Dict): context object
+    """
+
+    print("Received SQS event")
+    boto3_session = boto3.Session()
+    sqs_client = boto3_session.client("sqs")
+
+    queue = StandardSQS(
+        queue_url=WORKOUT_SCRAPE_TRIGGER_QUEUE_URL, sqs_client=sqs_client
+    )
+
+    if not NUMBER_WORKOUT_LINKS_PER_MESSAGE:
+        raise ValueError(
+            "'NUMBER_WORKOUT_LINKS_PER_MESSAGE' environment variable not set"
+        )
+
+    print(f"Putting message on queue: '{WORKOUT_SCRAPE_TRIGGER_QUEUE_URL}'")
+    print(
+        f"Number of workout links per message: {NUMBER_WORKOUT_LINKS_PER_MESSAGE}"
+    )
+    queue.send_message(message_body=NUMBER_WORKOUT_LINKS_PER_MESSAGE)
+
+    print("Finished scraping workout links")

ce14b7cc85ee5ee0cf26c21198877d9712e90c3c
Dan <daniel.lenehan.work@gmail.com>
2023-10-02 11:20:00 +0100

    New Queue urls

--- sheiva_cloud/sheiva_aws/sqs/__init__.py
+++ sheiva_cloud/sheiva_aws/sqs/__init__.py
@@ -8,6 +8,6 @@ from sheiva_cloud.sheiva_aws.sqs.classes import (
 )
 
 BASE_URL = "https://sqs.eu-west-1.amazonaws.com/381528172721"
-WORKOUTLINK_QUEUE_URL = f"{BASE_URL}/WorkoutLinkQueue"
-WORKOUTLINK_DEADLETTER_QUEUE_URL = f"{BASE_URL}/WorkoutLinkQueueDeadLetter"
+WORKOUTLINK_QUEUE_URL = f"{BASE_URL}/WorkoutScraperQueue"
+WORKOUTLINK_DEADLETTER_QUEUE_URL = f"{BASE_URL}/WorkoutScraperDeadLetterQueue"
 WORKOUT_SCRAPE_TRIGGER_QUEUE_URL = f"{BASE_URL}/WorkoutScrapeTriggerQueue"
